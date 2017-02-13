3064feaa7fe256bd79d25192229943b96670d080
Tettrox <tettroxx@gmail.com>
2017-02-13 00:50:55 -0600

    Changed AnimationSet to ASSpecial to comply with the new functionality of how animations work.

--- Scripts/Buildings.js
+++ Scripts/Buildings.js
@@ -16,9 +16,13 @@ class Building_CampFire extends Building
 	constructor()
 	{
 		super();
-		EntityTag = "CampFire";
+		this.EntityTag = "CampFire";
+		this.EntityWidth = 16;
+		this.EntityHeight = 16;
 		this.EntitySpr = document.getElementById("SprSheet_CampFire");
-		this.AnimationFrames = [new Point(), new Point()];
+		this.ASSpecial = [new Point(0, 0), new Point(16, 0), new Point(32, 0), new Point(48, 0)];
+		this.AnimationTimer = setInterval(this.doAnimation.bind(this), 80);
+		this.AnimationSet = this.ASSpecial;
 		this.BC_Bark = 2;
 	}
 }
